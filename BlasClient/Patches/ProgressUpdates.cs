df4df24160e7f66496170e0337debab316f9a374
Branden Wood <brandenek03912@hotmail.com>
2023-03-05 16:57:47 -0500

    Hopefully fix platform bug

--- BlasClient/Patches/ProgressUpdates.cs
+++ BlasClient/Patches/ProgressUpdates.cs
@@ -482,23 +482,29 @@ namespace BlasClient.Patches
     [HarmonyPatch(typeof(StraightMovingPlatform), "Use")]
     public class MovingPlatformUse_Patch
     {
-        public static void Postfix(StraightMovingPlatform __instance)
+        public static void Postfix(StraightMovingPlatform __instance, ref bool ____running)
         {
             if (ProgressManager.updatingProgress) return;
 
             string persistentId = __instance.GetPersistenID();
             Main.Multiplayer.Log("Activated platform: " + persistentId);
             Main.Multiplayer.progressManager.usePersistentObject(persistentId);
+
+            // Make sure this platform is set to running when activated twice
+            if (PersistentStates.getObjectSceneIndex(Core.LevelManager.currentLevel.LevelName, persistentId) >= 0)
+            {
+                ____running = true;
+            }
         }
     }
     [HarmonyPatch(typeof(StraightMovingPlatform), "GetCurrentPersistentState")]
     public class MovingPlatformReceive_Patch
     {
-        public static bool Prefix(string dataPath, StraightMovingPlatform __instance, ref bool ____running)
+        public static bool Prefix(string dataPath, StraightMovingPlatform __instance)
         {
             if (dataPath != "use") return true;
 
-            ____running = false;
+            __instance.Reset();
             __instance.Use();
             return false;
         }
@@ -506,7 +512,7 @@ namespace BlasClient.Patches
     [HarmonyPatch(typeof(StraightMovingPlatform), "SetCurrentPersistentState")]
     public class MovingPlatformLoad_Patch
     {
-        public static bool Prefix(PersistentManager.PersistentData data, StraightMovingPlatform __instance, ref bool ____running, string ___OnDestination)
+        public static bool Prefix(PersistentManager.PersistentData data, StraightMovingPlatform __instance)
         {
             if (data != null)
             {
@@ -514,8 +520,8 @@ namespace BlasClient.Patches
                 return !Main.Multiplayer.checkPersistentObject(__instance.GetPersistenID());
             }
 
-            ____running = false;
-            __instance.Use(); // Might have to change this
+            __instance.Reset();
+            __instance.Use();
             return false;
         }
     }
