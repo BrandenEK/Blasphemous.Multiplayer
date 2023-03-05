589ad99946d463220e192b102b993f35cf5c1390
Branden Wood <brandenek03912@hotmail.com>
2023-03-05 17:19:28 -0500

    Fix death anim bug

--- BlasClient/MonoBehaviours/EventReceiver.cs
+++ BlasClient/MonoBehaviours/EventReceiver.cs
@@ -4,6 +4,22 @@ namespace BlasClient.MonoBehaviours
 {
     public class EventReceiver : MonoBehaviour
     {
+        private Animator anim;
+
+        private void Start()
+        {
+            anim = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            AnimatorStateInfo state = anim.GetCurrentAnimatorStateInfo(0);
+            if (state.normalizedTime >= 0.95f && (state.IsName("Death") || state.IsName("Death Spike")))
+            {
+                anim.enabled = false;
+            }
+        }
+
         public void LaunchEvent(string eventName)
         {
             if (eventName == "INTERACTION_END")
