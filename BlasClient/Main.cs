dd8399074268b7d3c88f04017c65550b3db7d598
Branden Wood <brandenek03912@hotmail.com>
2023-03-18 18:24:27 -0400

    Update to api v1.3

--- BlasClient/Main.cs
+++ BlasClient/Main.cs
@@ -3,13 +3,13 @@
 namespace BlasClient
 {
     [BepInPlugin(MOD_ID, MOD_NAME, MOD_VERSION)]
-    [BepInDependency("com.damocles.blasphemous.modding-api", "1.2.0")]
+    [BepInDependency("com.damocles.blasphemous.modding-api", "1.3.0")]
     [BepInProcess("Blasphemous.exe")]
     public class Main : BaseUnityPlugin
     {
         public const string MOD_ID = "com.damocles.blasphemous.multiplayer";
         public const string MOD_NAME = "Multiplayer";
-        public const string MOD_VERSION = "1.0.0";
+        public const string MOD_VERSION = "1.0.1";
 
         public static Multiplayer Multiplayer;
 
--- BlasClient/MultiplayerCommand.cs
+++ BlasClient/MultiplayerCommand.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using ModdingAPI;
+using ModdingAPI.Commands;
 
 namespace BlasClient
 {
