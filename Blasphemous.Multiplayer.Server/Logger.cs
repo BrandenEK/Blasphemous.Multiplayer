b072e4fcd87ca422cd84a6f706ca951e1ac0ad50
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-08-15 23:36:43 -0400

    server: add logger

---
+++ Blasphemous.Multiplayer.Server/Logger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Blasphemous.Multiplayer.Server;
+
+public static class Logger
+{
+    public static void Info(object message)
+    {
+        Console.ForegroundColor = ConsoleColor.White;
+        Print(message);
+    }
+
+    public static void Warn(object message)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Print(message);
+    }
+
+    public static void Error(object message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Print(message);
+    }
+
+    public static void Success(object message)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Print(message);
+    }
+
+    public static void Print(object message)
+    {
+        Console.WriteLine($"[{DateTime.UtcNow:HH:mm:ss}] {message}");
+    }
+}
