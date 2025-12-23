0ea80e5c58163023a672ba5fdfcb64cc62a23d45
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-12-23 18:12:03 -0500

    client: send ping command

---
+++ Blasphemous.Multiplayer.Client/Ping/PingManager.cs
@@ -0,0 +1,20 @@
+﻿using Blasphemous.ModdingAPI;
+using UnityEngine;
+
+namespace Blasphemous.Multiplayer.Client.Ping;
+
+public class PingManager
+{
+    public void ReceivePing(float time)
+    {
+        ModLog.Info(time + " --> " + Time.time);
+    }
+
+    public void OnUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            Main.Multiplayer.NetworkManager.SendPing(Time.time, 0);
+        }
+    }
+}
