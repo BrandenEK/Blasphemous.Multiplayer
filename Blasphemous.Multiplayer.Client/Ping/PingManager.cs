25f538a0838a2029bc6716108b3819536f78c418
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-12-23 18:27:28 -0500

    client: calculate average ping

--- Blasphemous.Multiplayer.Client/Ping/PingManager.cs
+++ Blasphemous.Multiplayer.Client/Ping/PingManager.cs
@@ -1,13 +1,35 @@
 ﻿using Blasphemous.ModdingAPI;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Blasphemous.Multiplayer.Client.Ping;
 
 public class PingManager
 {
+    private readonly List<float> _delays = new(10);
+
+    private ushort AveragePing
+    {
+        get
+        {
+            if (_delays.Count == 0)
+                return 0;
+
+            float average = _delays.Sum() / _delays.Count;
+            return (ushort)(average * 1000);
+        }
+    }
+
     public void ReceivePing(float time)
     {
-        ModLog.Info(time + " --> " + Time.time);
+        float delay = Time.time - time;
+
+        _delays.Add(delay);
+        if (_delays.Count > MAX_DELAYS)
+            _delays.RemoveAt(0);
+
+        ModLog.Warn($"Average ping is {AveragePing} ms");
     }
 
     public void OnUpdate()
@@ -17,4 +39,6 @@ public class PingManager
             Main.Multiplayer.NetworkManager.SendPing(Time.time, 0);
         }
     }
+
+    private const int MAX_DELAYS = 10;
 }
