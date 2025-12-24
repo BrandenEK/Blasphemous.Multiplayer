e68873e99d6bdc1cf3301061678d6af8ad81d638
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-12-23 22:17:52 -0500

    ui: add color to the ping text

--- Blasphemous.Multiplayer.Client/Ping/PingDisplay.cs
+++ Blasphemous.Multiplayer.Client/Ping/PingDisplay.cs
@@ -1,4 +1,5 @@
 ﻿using Blasphemous.ModdingAPI.Helpers;
+using System.Linq;
 using UnityEngine;
 
 namespace Blasphemous.Multiplayer.Client.Ping;
@@ -15,8 +16,23 @@ public class PingDisplay : MonoBehaviour
 
     private void PingWindow(int windowID)
     {
-        string text = $"Ping: {Main.Multiplayer.PingManager.AveragePing}ms";
+        ushort ping = Main.Multiplayer.PingManager.AveragePing;
+        string color = _regions.OrderBy(x => x.MaxPing).First(x => ping <= x.MaxPing).Color;
+        string text = $"Ping: <color=#{color}>{Main.Multiplayer.PingManager.AveragePing}ms</color>";
 
-        GUI.Label(new Rect(10, 20, 80, 30), text);
+        var style = new GUIStyle(GUI.skin.label)
+        {
+            richText = true
+        };
+
+        GUI.Label(new Rect(10, 20, 80, 30), text, style);
     }
+
+    private static readonly PingRegion[] _regions =
+    {
+        new PingRegion(50, "109748"),
+        new PingRegion(100, "FFE733"),
+        new PingRegion(500, "FF8C01"),
+        new PingRegion(int.MaxValue, "ED2938"),
+    };
 }
