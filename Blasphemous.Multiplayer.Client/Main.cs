f4ad64d94f5418ee124378dd0fa23256d2cc0d94
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-02-05 11:08:02 -0500

    core: upgrade dependencies

--- Blasphemous.Multiplayer.Client/Main.cs
+++ Blasphemous.Multiplayer.Client/Main.cs
@@ -5,8 +5,8 @@ using Blasphemous.Multiplayer.Client.Ping;
 namespace Blasphemous.Multiplayer.Client;
 
 [BepInPlugin(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_VERSION)]
-[BepInDependency("Blasphemous.ModdingAPI", "2.5.0")]
-[BepInDependency("Blasphemous.CheatConsole", "1.0.1")]
+[BepInDependency("Blasphemous.ModdingAPI", "3.0.0")]
+[BepInDependency("Blasphemous.CheatConsole", "1.1.0")]
 internal class Main : BaseUnityPlugin
 {
     public static Multiplayer Multiplayer { get; private set; }
