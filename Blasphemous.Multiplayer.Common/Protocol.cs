aff02e927ea93ed1b88fa2e42fe337e188927608
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-02-05 14:51:53 -0500

    common: add default ip and port

--- Blasphemous.Multiplayer.Common/Protocol.cs
+++ Blasphemous.Multiplayer.Common/Protocol.cs
@@ -4,4 +4,14 @@ public static class Protocol
 {
     // Maybe use this instead of two separate ones
     public const int VERSION = 0;
+
+    /// <summary>
+    /// The default ip used by client and server
+    /// </summary>
+    public const string DEFAULT_IP = "localhost";
+
+    /// <summary>
+    /// The default port used by client and server
+    /// </summary>
+    public const int DEFAULT_PORT = 27051;
 }
