a5dc2e1b665c38cf1c12652ae9e39122fbc9c72c
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-02-05 13:40:05 -0500

    common: add refusal type

---
+++ Blasphemous.Multiplayer.Common/Enums/RefusalType.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Blasphemous.Multiplayer.Common.Enums;
+
+public enum RefusalType
+{
+    Connection = 0,
+    Protocol = 1,
+    DuplicateIp = 2,
+    DuplicateName = 3,
+    Password = 4,
+    PlayerLimit = 5,
+    Banned = 6,
+}
