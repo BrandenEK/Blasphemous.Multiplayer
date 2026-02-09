4313128976c9676d08c517092d5a995c2f0dad8d
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-02-09 17:30:40 -0500

    server: add basic functionality

--- Blasphemous.Multiplayer.Server/ServerHandler.cs
+++ Blasphemous.Multiplayer.Server/ServerHandler.cs
@@ -1,6 +1,8 @@
 ﻿using Basalt.Framework.Networking;
 using Basalt.Framework.Networking.Server;
 using Blasphemous.Multiplayer.Common;
+using Blasphemous.Multiplayer.Server.Models;
+using System.Collections.Generic;
 using System.Net.Sockets;
 using System.Threading;
 
@@ -39,6 +41,7 @@ public class ServerHandler
             return false;
         }
 
+        _connectedPlayers.Clear(); // TODO: this was old as well
         return true;
     }
 
@@ -49,7 +52,16 @@ public class ServerHandler
 
     private void OnClientDisconnected(string ip)
     {
-        Logger.Info($"Client disconnected at {ip}");
+        Logger.Info($"Client disconnected at {ip}"); // TODO: everything after this was old
+
+        // Make sure this client was actually connected, not just rejected from server
+        if (!_connectedPlayers.ContainsKey(ip))
+            return;
+
+        // Send that this player has disconnected & remove them
+        sendPlayerConnection(e.ip, false);
+        _connectedPlayers.Remove(ip);
+        Core.removeUnusedGameData(_connectedPlayers);
     }
 
     private void OnPacketReceived(string ip, BasePacket packet)
@@ -84,4 +96,24 @@ public class ServerHandler
     }
 
     private const int READ_INTERVAL_MS = 16;
+
+    // =======================================
+    // New (old) stuff that I want to refactor
+    // =======================================
+
+    private Dictionary<string, PlayerInfo> _connectedPlayers = [];
+
+    public Dictionary<string, PlayerInfo> GetPlayers()
+    {
+        return _connectedPlayers;
+    }
+
+    private PlayerInfo getCurrentPlayer(string ip)
+    {
+        if (_connectedPlayers.ContainsKey(ip))
+            return _connectedPlayers[ip];
+
+        Logger.Warn("Data for " + ip + " has not been created yet!");
+        return new PlayerInfo(string.Empty, 1);
+    }
 }
