7a477f0d3a209260b5df5243d3aec08984dd3367
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-02-09 15:31:05 -0500

    common: add position packets

---
+++ Blasphemous.Multiplayer.Common/Packets/PositionPacket.cs
@@ -0,0 +1,10 @@
+﻿using Basalt.Framework.Networking;
+
+namespace Blasphemous.Multiplayer.Common.Packets;
+
+public class PositionPacket(float x, float y) : BasePacket
+{
+    public float X { get; set; } = x;
+
+    public float Y { get; set; } = y;
+}
---
+++ Blasphemous.Multiplayer.Common/Packets/PositionResponsePacket.cs
@@ -0,0 +1,12 @@
+﻿using Basalt.Framework.Networking;
+
+namespace Blasphemous.Multiplayer.Common.Packets;
+
+public class PositionResponsePacket(string player, float x, float y) : BasePacket
+{
+    public string Player { get; set; } = player;
+
+    public float X { get; set; } = x;
+
+    public float Y { get; set; } = y;
+}
