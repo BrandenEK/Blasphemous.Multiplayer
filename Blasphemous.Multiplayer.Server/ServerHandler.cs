141cc34d03ea795526f0e31c0c92bc19df4f9d76
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-02-09 15:24:22 -0500

    server: remove old temp code

--- Blasphemous.Multiplayer.Server/ServerHandler.cs
+++ Blasphemous.Multiplayer.Server/ServerHandler.cs
@@ -1,7 +1,6 @@
 ﻿using Basalt.Framework.Networking;
 using Basalt.Framework.Networking.Serializers;
 using Basalt.Framework.Networking.Server;
-using Blasphemous.Multiplayer.Server.TempCommon;
 using System.Net.Sockets;
 using System.Threading;
 
@@ -21,9 +20,11 @@ public class ServerHandler
         _maxPlayers = maxPlayers;
         _password = password;
 
-        _server = new NetworkServer(new ClassicSerializer()
-            .RegisterPacket<PositionPacket>(0, () => new PositionPacket(0, 0))
-            .RegisterPacket<ScenePacket>(5, () => new ScenePacket(string.Empty)));
+        //_server = new NetworkServer(new ClassicSerializer()
+        //    .RegisterPacket<PositionPacket>(0, () => new PositionPacket(0, 0))
+        //    .RegisterPacket<ScenePacket>(5, () => new ScenePacket(string.Empty)));
+
+        _server = new NetworkServer(null);
 
         _server.OnClientConnected += OnClientConnected;
         _server.OnClientDisconnected += OnClientDisconnected;
--- Blasphemous.Multiplayer.Server/TempCommon/ScenePacket.cs
+++
@@ -1,8 +0,0 @@
-﻿using Basalt.Framework.Networking;
-
-namespace Blasphemous.Multiplayer.Server.TempCommon;
-
-public class ScenePacket(string scene) : BasePacket
-{
-    public string Scene { get; set; } = scene;
-}
