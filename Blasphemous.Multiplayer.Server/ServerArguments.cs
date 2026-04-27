db7c7f1411c38e54a61bb1875fd34bb00d2dbb92
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-04-27 13:37:28 -0400

    server: redo arguments

--- Blasphemous.Multiplayer.Server/ServerArguments.cs
+++ Blasphemous.Multiplayer.Server/ServerArguments.cs
@@ -1,16 +1,17 @@
 ﻿using Basalt.CommandParser;
+using Basalt.CommandParser.Attributes;
 using Blasphemous.Multiplayer.Common;
 
 namespace Blasphemous.Multiplayer.Server;
 
 public class ServerArguments : ProgramArguments
 {
-    [IntegerArgument('p', "port")]
+    [IntegerArgument("port", "p", "Which port the server will listen on (1-65535")]
     public int Port { get; set; } = Protocol.DEFAULT_PORT;
 
-    [IntegerArgument('m', "max-players")]
+    [IntegerArgument("max-players", "mp", "The maximum number of players allowed in the server")]
     public int MaxPlayers { get; set; } = 10;
 
-    [StringArgument('s', "password")]
+    [StringArgument("password", "ps", "The password to use")]
     public string Password { get; set; } = string.Empty;
 }
