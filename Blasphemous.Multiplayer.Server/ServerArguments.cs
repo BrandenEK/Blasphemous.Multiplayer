8b85835505737af3d1e30b1fc77a12eb38c693f9
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-04-27 13:41:31 -0400

    server: implement new command parser (#107)
    * server: upgrade command parser
    
    * server: rename arguments data
    
    * server: redo arguments
    
    * server: process new arguments

---
+++ Blasphemous.Multiplayer.Server/ServerArguments.cs
@@ -0,0 +1,17 @@
+﻿using Basalt.CommandParser;
+using Basalt.CommandParser.Attributes;
+using Blasphemous.Multiplayer.Common;
+
+namespace Blasphemous.Multiplayer.Server;
+
+public class ServerArguments : ProgramArguments
+{
+    [IntegerArgument("port", "p", "Which port the server will listen on (1-65535)")]
+    public int Port { get; set; } = Protocol.DEFAULT_PORT;
+
+    [IntegerArgument("max-players", "mp", "The maximum number of players allowed in the server")]
+    public int MaxPlayers { get; set; } = 10;
+
+    [StringArgument("password", "ps", "The password to use")]
+    public string Password { get; set; } = string.Empty;
+}
